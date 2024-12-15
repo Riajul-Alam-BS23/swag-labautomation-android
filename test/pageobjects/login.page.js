873932599355bdbe4b55d10ca54c458e52b6ee79
Md. Riajul Alam Bhuiyan <riajul.alam@brainstation-23.com>
2024-12-15 15:08:46 +0600

    refactor

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,49 +1,4 @@
-// import { $ } from '@wdio/globals'
-// import Page from './page.js';
-
-// /**
-//  * sub page containing specific selectors and methods for a specific page
-//  */
-// class LoginPage extends Page {
-//     /**
-//      * define selectors using getter methods
-//      */
-//     get inputUsername () {
-//         return $('#username');
-//     }
-
-//     get inputPassword () {
-//         return $('#password');
-//     }
-
-//     get btnSubmit () {
-//         return $('button[type="submit"]');
-//     }
-
-//     /**
-//      * a method to encapsule automation code to interact with the page
-//      * e.g. to login using username and password
-//      */
-//     async login (username, password) {
-//         await this.inputUsername.setValue(username);
-//         await this.inputPassword.setValue(password);
-//         await this.btnSubmit.click();
-//     }
-
-//     /**
-//      * overwrite specific options to adapt it to page object
-//      */
-//     open () {
-//         return super.open('login');
-//     }
-// }
-
-// export default new LoginPage();
-
-
-// login.page.js
 class LoginPage {
-    // Locators
     get usernameField() {
       return $("~test-Username");
     }
@@ -60,7 +15,6 @@ class LoginPage {
       return $('android=new UiSelector().text("Sorry, this user has been locked out.")');
     }
   
-    // Methods
     async scrollToUserList() {
       await driver.execute("mobile: scroll", {
         strategy: "accessibility id",
--- test/specs/test.js
+++ test/specs/test.js
@@ -6,20 +6,9 @@ describe('Swag Labs Mobile App Automation', async () => {
 
     describe("Verify Locked Out User", () => {
       it("should display an error message for a locked-out user", async () => {
-        // Step 1: Activate the Swag Labs app
         await driver.activateApp("com.swaglabsmobileapp");
-    
-        // Step 2: Log in as 'locked_out_user'
         await LoginPage.loginAsLockedOutUser();
-    
-        // Step 3: Assert the locked-out error message
         await expect(LoginPage.errorMessage).toBeDisplayed();
-    
-        // // Optional: Print the error message for debugging
-        // const errorText = await LoginPage.getErrorMessageText();
-        // console.log("Error Message: ", errorText);
-    
-        // Verification
         expect(errorText).toBe("Sorry, this user has been locked out.");
       });
     });    
